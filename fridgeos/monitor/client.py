2fdb767872c5b6ad2731e3b98c8d50a46e8a3792
Adam McCaughan <amccaugh@users.noreply.github.com>
2025-05-05 16:18:31 -0600

    Remove double return from monitor client

--- fridgeos/monitor/client.py
+++ fridgeos/monitor/client.py
@@ -10,16 +10,9 @@ class MonitorClient:
         """ Get the metrics from the monitor server as a dictionary.
         Optionally, specify which subset of metrics (e.g. temperatures,
         heater_max_values) by specifying `name """
-        try:
-            response = requests.get(self.url, timeout = self.timeout)
-            metrics_dict = json.loads(response.text)
-            if name is not None:
-                return metrics_dict[name], True
-            else:
-                return metrics_dict, True
-        except requests.exceptions.ReadTimeout:
-            print(f"Request to {self.url} timed out after {self.timeout} seconds")
-            return None, False
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return None, False
+        response = requests.get(self.url, timeout = self.timeout)
+        metrics_dict = json.loads(response.text)
+        if name is not None:
+            return metrics_dict[name]
+        else:
+            return metrics_dict
