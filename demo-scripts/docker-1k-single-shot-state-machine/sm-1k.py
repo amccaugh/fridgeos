d9ca3413675f68d82d99ab5515a37b352b369d2b
morgensternR <49378275+morgensternR@users.noreply.github.com>
2025-05-09 15:30:00 -0600

    update 1m-1k.py and added docker network to compose

--- demo-scripts/docker-1k-single-shot-state-machine/sm-1k.py
+++ demo-scripts/docker-1k-single-shot-state-machine/sm-1k.py
@@ -1,19 +1,13 @@
 #%%
-#from fridgeos import MonitorServer, MonitorClient, HALServer, HALClient, crc_gl4
 from fridgeos import HALClient
 from fridgeos import MonitorClient
-from fridgeos.single_shot_1k import Single_shot_1k
-
-# Define server and client IPs and ports
-ip = '127.0.0.1'
-http_port = 8000
-hal_port = 5555
+from fridgeos.statemachine.single_shot_1k import Single_shot_1k
 
 
 # Make a monitor client for the state machine to get metrics
-sm_monitor_client = MonitorClient(url=f'http://localhost:{http_port}/', timeout=3)
+sm_monitor_client = MonitorClient(url=f'http://monitor:8000/', timeout=3)
 # Make a HAL client for the state machine to set heater values
-sm_hal_client = HALClient(ip=ip, port=hal_port)
+sm_hal_client = HALClient(ip='hal', port=5555)
 # Make a state machine and input its required params
 sm_settings_toml = './config/single_shot_1k_sm_config.toml'
 sm = Single_shot_1k(settings_toml = sm_settings_toml, 
