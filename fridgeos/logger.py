92e30dc0a30b40528e4498b7c2bda2abcef104f6
Adam McCaughan <amccaugh@users.noreply.github.com>
2025-07-14 12:47:13 -0600

    Update logger.py

--- fridgeos/logger.py
+++ fridgeos/logger.py
@@ -1,4 +1,5 @@
 import logging
+import logging.handlers
 import sys
 import os
 
@@ -7,28 +8,36 @@ class FridgeLogger:
         self.logger = self.setup_logging(log_path, debug, logger_name)
 
     def setup_logging(self, log_path, debug=False, logger_name='HAL'):
-        # Make the logging path if doesn't exist
         if log_path is not None:
             log_dir = os.path.dirname(log_path)
             if not os.path.exists(log_dir):
                 os.makedirs(log_dir)
-        # Create the logger
         logger = logging.getLogger(logger_name)
         logger.setLevel(logging.DEBUG)
-        # Create 3 "handlers" two which output to a log file, the other to stdout
+
+        # Use RotatingFileHandler for log rotation
+        max_bytes = 10 * 1024 * 1024  # 10 MB
+        backup_count = 5  # Number of backup files to keep
+
         handler1 = logging.StreamHandler(sys.stdout)
-        handler2 = logging.FileHandler(os.path.join(log_path, f'{logger_name.lower()}-errors.log'), mode='a')
-        handler3 = logging.FileHandler(os.path.join(log_path, f'{logger_name.lower()}-debug.log'), mode='a')
+        handler2 = logging.handlers.RotatingFileHandler(
+            os.path.join(log_path, f'{logger_name.lower()}-errors.log'),
+            mode='a', maxBytes=max_bytes, backupCount=backup_count
+        )
+        handler3 = logging.handlers.RotatingFileHandler(
+            os.path.join(log_path, f'{logger_name.lower()}-debug.log'),
+            mode='a', maxBytes=max_bytes, backupCount=backup_count
+        )
         handler1.setLevel(logging.INFO)
         handler2.setLevel(logging.INFO)
         handler3.setLevel(logging.DEBUG)
-        # Set the format of the log messages
+
         log_date_format = '%Y-%m-%d %H:%M:%S'
         format = logging.Formatter(fmt = '%(asctime)s.%(msecs)03d,\t%(levelname)s,\t%(message)s', datefmt=log_date_format)
         handler1.setFormatter(format)
         handler2.setFormatter(format)
         handler3.setFormatter(format)
-        # Create the logger
+
         logger.addHandler(handler1)
         if log_path is not None:
             logger.addHandler(handler2)
