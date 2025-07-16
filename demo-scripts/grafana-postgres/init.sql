9e49ef1b4272f1012bb3906773b51227e7cb89d4
Adam McCaughan <amccaugh@users.noreply.github.com>
2025-07-16 09:24:46 -0600

    Update

--- demo-scripts/grafana-postgres/init.sql
+++ demo-scripts/grafana-postgres/init.sql
@@ -1,7 +1,14 @@
-CREATE TABLE cryostat_temperatures (
+CREATE TABLE temperatures (
     time TIMESTAMPTZ NOT NULL,
-    cryostatname TEXT NOT NULL,
+    fridgename TEXT NOT NULL,
     sensorname TEXT NOT NULL,
     temperature REAL NOT NULL);
-SELECT create_hypertable('cryostat_temperatures', 'time');
-CREATE INDEX idx_cryostat ON cryostat_temperatures(cryostatname);
+SELECT create_hypertable('temperatures', 'time');
+CREATE INDEX idx_cryostat ON temperatures(fridgename);
+
+-- Create fridgeosuser
+CREATE USER fridgeosuser WITH PASSWORD '${FRIDGEOS_PASSWORD}';
+GRANT CONNECT ON DATABASE fridgedb TO fridgeosuser;
+GRANT USAGE ON SCHEMA public TO fridgeosuser;
+GRANT SELECT, INSERT ON ALL TABLES IN SCHEMA public TO fridgeosuser;
+GRANT SELECT, INSERT ON TABLE cryostat_temperatures TO fridgeosuser;
