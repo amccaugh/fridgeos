a8b57562913712fb44009189201b41af7d1eb6c2
Adam McCaughan <amccaugh@users.noreply.github.com>
2025-07-17 15:02:20 -0600

    Change MonitorServer style

--- fridgeos/fridgeos/monitor/server.py
+++ fridgeos/fridgeos/monitor/server.py
@@ -4,7 +4,7 @@ from fridgeos.statemachine.client import StateMachineClient
 from fridgeos.logger import FridgeLogger
 from fridgeos.metricserver import MetricServer
 
-class MonitorServer:
+class MonitorServer(MetricServer):
     def __init__(self,
                  http_port,
                  hal_ip,
@@ -19,7 +19,7 @@ class MonitorServer:
         values/state
         """
         self.logger = FridgeLogger(log_path="logs", debug=True, logger_name="Monitor").logger
-        self.metrics_server = MetricServer(ip_address="0.0.0.0", port=http_port)
+        super().__init__(ip_address="0.0.0.0", port=http_port)
         self.hal_client = HALClient(ip = hal_ip, port = hal_port)
         self.statemachine_client = StateMachineClient(ip = statemachine_ip, port = statemachine_port)
         self.min_update_period = min_update_period # seconds
@@ -29,20 +29,20 @@ class MonitorServer:
     def update(self):
         # Get temperatures from HAL
         temperatures = self.hal_client.get_temperatures()
-        self.metrics_server.update_metric_values(metric_name = 'temperatures',
-                                                 new_values_dict = temperatures)
+        self.update_metric_values(metric_name = 'temperatures',
+                                   new_values_dict = temperatures)
         self.logger.debug(f'Updated temperatures: {list(temperatures.keys())}')
 
         # Get heater values from HAL
         heater_values = self.hal_client.get_heater_values()
-        self.metrics_server.update_metric_values(metric_name = 'heaters',
-                                                 new_values_dict = heater_values)
+        self.update_metric_values(metric_name = 'heaters',
+                                   new_values_dict = heater_values)
         self.logger.debug(f'Updated heaters: {list(heater_values.keys())}')
 
         # Get heater max values from HAL
         heater_max_values = self.hal_client.get_heater_max_values()
-        self.metrics_server.update_metric_values(metric_name = 'heater_max_values',
-                                                 new_values_dict = heater_max_values)
+        self.update_metric_values(metric_name = 'heater_max_values',
+                                   new_values_dict = heater_max_values)
 
         # Get state from StateMachine
         try:
@@ -51,13 +51,13 @@ class MonitorServer:
                 state = state_response['state']
             else:
                 state = str(state_response)
-            self.metrics_server.update_metric_values(metric_name = 'state',
-                                                     new_values_dict = state)
+            self.update_metric_values(metric_name = 'state',
+                                       new_values_dict = state)
             self.logger.debug(f'Updated state: {state}')
         except Exception as e:
             self.logger.warning(f'Failed to get state from StateMachine: {e}')
-            self.metrics_server.update_metric_values(metric_name = 'state',
-                                                     new_values_dict = 'unknown')
+            self.update_metric_values(metric_name = 'state',
+                                       new_values_dict = 'unknown')
 
     def run(self):
         self.logger.info('Starting monitor server loop')
