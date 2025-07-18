797cbf6dba288f2cb5a1a55b46a7cdc17938bd7e
Adam McCaughan <amccaugh@users.noreply.github.com>
2025-07-18 10:29:18 -0600

    Update monitor-server.py

--- fridgeos-docker/scripts/monitor-server.py
+++ fridgeos-docker/scripts/monitor-server.py
@@ -9,24 +9,27 @@ hal_client = HALClient(ip='hal', port=5555)
 statemachine_client = StateMachineClient(ip='statemachine', port=5556)
 
 
-class DummyHALClient:
-    def get_temperatures(self):
-        return {'T1': 1.23 + random.uniform(-0.1, 0.1), 'T2': 4.56 + random.uniform(-0.1, 0.1)}
-    def get_heater_values(self):
-        return {'H1': 0.1 + random.uniform(-0.1, 0.1), 'H2': 0.2 + random.uniform(-0.1, 0.1)}
-    def get_heater_max_values(self):
-        return {'H1': 1.0, 'H2': 1.0}
+# class DummyHALClient:
+#     def get_temperatures(self):
+#         print("DummyHALClient.get_temperatures() called")
+#         return {'T1': 1.23 + random.uniform(-0.1, 0.1), 'T2': 4.56 + random.uniform(-0.1, 0.1)}
+#     def get_heater_values(self):
+#         print("DummyHALClient.get_heater_values() called")
+#         return {'H1': 0.1 + random.uniform(-0.1, 0.1), 'H2': 0.2 + random.uniform(-0.1, 0.1)}
+#     def get_heater_max_values(self):
+#         print("DummyHALClient.get_heater_max_values() called")
+#         return {'H1': 1.0, 'H2': 1.0}
 
-# Dummy StateMachineClient
-class DummyStateMachineClient:
-    def get_state(self):
-        if random.random() < 0.5:
-            return {'state': 'cold'}
-        else:
-            return {'state': 'warm'}
+# # Dummy StateMachineClient
+# class DummyStateMachineClient:
+#     def get_state(self):
+#         if random.random() < 0.5:
+#             return {'state': 'cold'}
+#         else:
+#             return {'state': 'warm'}
 
-hal_client = DummyHALClient()
-statemachine_client = DummyStateMachineClient()
+# hal_client = DummyHALClient()
+# statemachine_client = DummyStateMachineClient()
 
 monitor_server = MonitorServer(http_port=8000,
                                hal_client=hal_client,
