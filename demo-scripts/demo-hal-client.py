4db6c96ff545cf28ee3150a03508383f049e98c6
Adam McCaughan <amccaugh@users.noreply.github.com>
2025-07-23 10:08:13 -0600

    Update demo-hal-client.py

--- demo-scripts/demo-hal-client.py
+++ demo-scripts/demo-hal-client.py
@@ -1,8 +1,8 @@
 #%%
 from fridgeos import HALClient
 
-halclient = HALClient(ip = '127.0.0.1', port = 5555)
+halclient = HALClient(ip = '127.0.0.1', port = 8000)
 print(halclient.get_heater_values())
 print(halclient.get_temperatures())
 print(halclient.get_temperature('1K'))
-print(halclient.set_heater_value('1K-switch', 0.1))
+print(halclient.set_heater_value('pump', 0.1))
