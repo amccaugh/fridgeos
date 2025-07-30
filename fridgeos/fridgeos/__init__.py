1fd9d71936523eace3189321b107b494f61d5d36
Adam McCaughan <amccaugh@users.noreply.github.com>
2025-07-30 12:45:21 -0600

    refactor scraper

--- fridgeos/fridgeos/__init__.py
+++ fridgeos/fridgeos/__init__.py
@@ -1,5 +1,5 @@
 from fridgeos.hal import HALServer, HALClient
-from fridgeos.scraper.scraper import Scraper, PostgresUploader
+from fridgeos.scraper import Scraper, PostgresUploader
 from fridgeos.statemachine import StateMachineServer, StateMachineClient
 # from fridgeos.statemachine import single_shot_1k
 # from fridgeos.statemachine import continous_dr
