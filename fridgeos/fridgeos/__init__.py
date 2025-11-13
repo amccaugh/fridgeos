205685e65864d0ade5a3a2b2ecee214e2dab0938
Adam McCaughan <amccaugh@users.noreply.github.com>
2025-11-12 17:54:13 -0700

    Update __init__.py

--- fridgeos/fridgeos/__init__.py
+++ fridgeos/fridgeos/__init__.py
@@ -5,5 +5,3 @@ __version__ = "1.0.0"
 from fridgeos.hal import HALServer, HALClient
 from fridgeos.scraper import Scraper, PostgresUploader
 from fridgeos.statemachine import StateMachineServer, StateMachineClient
-# from fridgeos.statemachine import single_shot_1k
-# from fridgeos.statemachine import continous_dr
